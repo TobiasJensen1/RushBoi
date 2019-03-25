b4292f46ec2d88076044405571f8406c60c251ee
tobbe <tobbejj@hotmail.com>
2019-03-25 15:54:06 +0100

    initial

---
+++ RushBoi/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveHorizontal;
+    float moveVertical;
+    bool isGrounded;
+    Vector2 movement;
+    public float speed;
+    public float jumpHeight;
+    Rigidbody2D rb;
+    Animator a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        rb = GetComponent<Rigidbody2D>();
+        a = GetComponent<Animator>();
+        a.Play("Run");
+        isGrounded = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        run();
+        jump();
+    }
+
+
+    void run()
+    {
+        movement = new Vector2(moveHorizontal + speed, 0);
+        rb.velocity = movement;
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Fire1") && isGrounded)
+        {
+            transform.DOMoveY(transform.position.y + jumpHeight, 0.4f);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+}
---
+++ RushBoi/Assets/Scripts/ProcedualGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcedualGenerator : MonoBehaviour
+{
+    public GameObject chunk1;
+    public GameObject chunk2;
+    public GameObject chunk3;
+    public GameObject chunkPos;
+    public GameObject oldChunk;
+
+    
+    
+
+    ArrayList chunks = new ArrayList();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chunks.Add(chunk1);
+        chunks.Add(chunk2);
+        chunks.Add(chunk3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        int rand = Random.Range(0, chunks.Count);
+        GameObject objectToPlace = (GameObject) chunks[rand];
+        print(rand);
+
+        //chunkPos = objectToPlace;
+
+
+        if (collision.transform.tag == "SpawnCollider")
+        {
+
+            print(oldChunk);
+            Destroy(oldChunk);
+            oldChunk = collision.gameObject.transform.parent.gameObject;
+            
+            GameObject newChunk = Instantiate(objectToPlace, new Vector2(chunkPos.transform.position.x + 23.5f, chunkPos.transform.position.y), Quaternion.identity);
+
+            chunkPos = newChunk;
+        }
+
+    }
+
+
+}
